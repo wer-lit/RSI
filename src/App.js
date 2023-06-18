c3f13797fdcdd3a24472bc4112b4be930e610aba
weronika <weronika.lit0@gmail.com>
2023-06-19 00:29:48 +0200

    react app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {BrowserRouter, BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import {Navbar, Footer} from "./components/layout";
+import {Home} from "./components/home";
+import {Products} from "./components/products";
+import {Authors} from "./components/authors";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <Navbar/>
+                <Routes>
+                    <Route path={"/"} element={<Home/>}/>
+                    <Route path={"/products"} element={<Products/>}/>
+                    <Route path={"/authors"} element={<Authors/>}/>
+
+                </Routes>
+
+                <Footer/>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
+
+
+
+
+
+
